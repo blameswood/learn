f2f5c3114a3f6ffadbc3a4c9de046217fa9a8ce5
sandy <blameswood@hotmail.com>
2017-06-27 16:28:43 +0800

    [modify]调整目录结构

---
+++ git-demo/src/main/java/com/sandy/gitdemo/Demo.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package com.sandy.gitdemo;
+
+/**
+ * @author Administrator
+ *
+ */
+public class Demo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+	}
+	
+	public static void test(){
+		//TODO
+	}
+
+}
